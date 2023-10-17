2a11c3ac28d7a3dc17340e131d0dba153af71abf
Ali İhsan Yüksel <alihsanyuksel@gmail.com>
2023-10-17 13:38:12 +0300

    Class componen with state, table with map function and server data, styled header

--- my-react-app/src/BookLibrary.js
+++ my-react-app/src/BookLibrary.js
@@ -3,16 +3,50 @@ import axios from "axios";
 
 class BookLibrary extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            books: [],
+        };
+    }
+
+
     componentDidMount(){
 
         axios(process.env.REACT_APP_SERVER_URL)
-            .then(result => console.log(result))
+            .then(result => this.setState({books: result.data}))
             .catch(error => console.log(error));
     }
 
 
     render() {
-        return (<div>Library</div>);
+        let books = this.state.books.map((book) => {
+        return (
+            <tr key={book.id}>
+                <td>{book.author}</td>
+                <td>{book.title}</td>
+                <td>{book.published}</td>
+            </tr>)
+        });
+        console.log('render', this.state.books); 
+        return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th>Published</th>   
+                </tr>
+                </thead>
+                <tbody>
+                    {books} 
+                </tbody>
+                
+            </table>
+        </div>
+        );
     }
 }
 
