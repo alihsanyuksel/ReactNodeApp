ea5fe16068be1fd5799ca003f7fb3e8aa83fe387
Ali İhsan Yüksel <alihsanyuksel@gmail.com>
2023-10-17 22:46:29 +0300

    node modules and react-router-dom 1

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import BookLibrary from './BookLibrary';
+import Book from './Book';
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
 
   return (
+    <Router>
     <div className="App">
       <Header />
-      <BookLibrary />
+
+      <Routes>
+      <Route exact path="/" element={<BookLibrary />}></Route>
+      <Route path="/create" element={<Book />}></Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
